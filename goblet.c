9570043ca7644367be34a9eceabdc4b2477f63e0
Baptiste-Madelaine <madelainebaptiste61@gmail.com>
2022-11-28 08:44:32 +0100

    Test

--- goblet.c
+++ goblet.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "board.h"
 
-#define DIMENSIONS 3
-#define NB_SIZE 3
-#define NB_INITIAL_PIECES 2
-#define NB_PLAYERS 2
+//#define DIMENSIONS 3
+//#define NB_SIZE 3
+//#define NB_INITIAL_PIECES 2
+//#define NB_PLAYERS 2
 
 int main(){
+    board game = new_game();
 
+    while (get_winner(game)== NO_PLAYER){
+
+    }
     return 0;
 }
